026dee5853fc75f3dce555653251ff934220d6a3
Stefan Vassilev <stevasvas@gmail.com>
2026-01-14 13:13:45 +0100

    removed unecessary documentation. Removed from_yaml function from ExperimentConfig class to be a standalone method

--- src/phishing_email_detector/utils/config.py
+++ src/phishing_email_detector/utils/config.py
@@ -61,41 +61,25 @@ class ExperimentConfig:
     train: TrainConfig
     output_dir: str = "results/"
     
-    """
-        The `from_yaml` class method in Python loads a configuration from a YAML file and creates an
-        `ExperimentConfig` object with specified data, model, and training configurations.
-        
-        :param cls: In the given code snippet, `cls` refers to the class itself. In this context, `cls` is
-        used as a reference to the class `ExperimentConfig`. The `from_yaml` method is a class method of
-        the `ExperimentConfig` class, and `cls` is used to refer
-        :param yaml_path: The `from_yaml` method is a class method that loads configuration settings from a
-        YAML file. It takes a `yaml_path` parameter, which is a string representing the path to the YAML
-        file containing the configuration
-        :type yaml_path: str
-        :return: An instance of the `ExperimentConfig` class is being returned, initialized with `data`,
-        `model`, and `train` configurations parsed from the YAML file specified by `yaml_path`. The `data`
-        configuration is parsed using `DataConfig`, and the `model` configuration is parsed based on the
-        `model_type` specified in the YAML file. In this case, if the `model_type`
-        """
-    @classmethod
-    def from_yaml(cls, yaml_path: str) -> "ExperimentConfig":
-        """Load config from YAML file."""
-        with open(yaml_path, 'r') as f:
-            cfg_dict = yaml.safe_load(f)
-        
-        # Parse each section
-        data = DataConfig(**cfg_dict.get("data", {}))
-        model_dict = cfg_dict.get("model", {})
-        model_type = model_dict.get("model_type", "transformer")
-        
-        if model_type == "fnn":
-            model = FNNConfig(**model_dict)
-        # elif model_type == "transformer":
-        #     model = TransformerConfig(**model_dict)
-        # elif model_type == "rnn":
-        #     model = RNNConfig(**model_dict)
-        # elif model_type == "lstm":
-        #     model = LSTMConfig(**model_dict)
+@classmethod
+def from_yaml(cls, yaml_path: str) -> "ExperimentConfig":
+    """Load config from YAML file."""
+    with open(yaml_path, 'r') as f:
+        cfg_dict = yaml.safe_load(f)
+    
+    # Parse each section
+    data = DataConfig(**cfg_dict.get("data", {}))
+    model_dict = cfg_dict.get("model", {})
+    model_type = model_dict.get("model_type", "transformer")
+    
+    if model_type == "fnn":
+        model = FNNConfig(**model_dict)
+    # elif model_type == "transformer":
+    #     model = TransformerConfig(**model_dict)
+    # elif model_type == "rnn":
+    #     model = RNNConfig(**model_dict)
+    # elif model_type == "lstm":
+    #     model = LSTMConfig(**model_dict)
 
         
         train = TrainConfig(**cfg_dict.get("train", {}))
