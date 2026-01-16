612259694c1b599f5758709888b81149775dda8c
Stefan Vassilev <stevasvas@gmail.com>
2026-01-16 11:24:22 +0100

    implemented smoke tests

---
+++ tests/test_models.py
@@ -0,0 +1,21 @@
+import pytest
+import tensorflow as tf
+from src.phishing_email_detector.models.registry import get_model
+from src.phishing_email_detector.utils.config import FNNConfig, TransformerConfig
+
+def test_fnn_build():
+    """Smoke test FNN model instantiation."""
+    config = FNNConfig()
+    model = get_model(config)
+    assert model.model is not None
+    assert len(model.model.layers) > 0
+
+def test_forward_pass():
+    """Test model forward pass on dummy data."""
+    config = FNNConfig()
+    model = get_model(config)
+    
+    # Dummy input
+    dummy_text = tf.constant(["This is a test email"])
+    output = model.model(dummy_text)
+    assert output.shape == (1, 1)
