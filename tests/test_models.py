cfdf798b751a07a660cdde911f996f5bc625719b
Stefan Vassilev <stevasvas@gmail.com>
2026-01-18 17:25:36 +0100

    Fix model compiling in FNN smoke tests by adding missing optimizer and learning rate parameters

--- tests/test_models.py
+++ tests/test_models.py
@@ -8,7 +8,7 @@ def test_fnn_build():
     config = FNNConfig()
     model = get_model(config)
     # assertion error if model is not compiled
-    model.compile()
+    model.compile(optimizer="adamw", learning_rate=0.001)
     assert model.model is not None
     assert len(model.model.layers) > 0
 
@@ -19,5 +19,6 @@ def test_forward_pass():
     
     # Dummy input
     dummy_text = tf.constant(["This is a test email"])
+    model.compile(optimizer="adamw", learning_rate=0.001)
     output = model.model(dummy_text)
     assert output.shape == (1, 1)
