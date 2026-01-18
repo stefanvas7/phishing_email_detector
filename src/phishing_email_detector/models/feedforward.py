f395a6df4e78eeea3b6c27abe2118aef2d6b6aae
Stefan Vassilev <stevasvas@gmail.com>
2026-01-18 17:46:42 +0100

    Add comment to clarify purpose of environment variable settings on macOS

--- src/phishing_email_detector/models/feedforward.py
+++ src/phishing_email_detector/models/feedforward.py
@@ -1,3 +1,4 @@
+# necessary to make function on Macos
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # Suppress TF info/warning logs
 os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'  # Prevent OOM by allocating GPU memory gradually
